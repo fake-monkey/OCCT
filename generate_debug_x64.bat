c60604c3f8268445d99d2068dd7d727322671408
FakeMonkey <msolwooden@outlook.com>
2023-05-03 09:38:36 +0800

    采用cmake机制进行open cascade的安装和引用。

--- generate_debug_x64.bat
+++ generate_debug_x64.bat
@@ -1,7 +1,9 @@
 mkdir build
-cmake -S . -B build -A x64 -D CMAKE_BUILD_TYPE=Debug ^
-    -D CMAKE_INSTALL_PREFIX:PATH="D:/Program Files/OCCT" ^
+cmake -S . -B build -A x64 -D CMAKE_BUILD_TYPE=Debug -D BUILD_LIBRARY_TYPE:STRING="Static" ^
+    -D CMAKE_INSTALL_PREFIX:PATH="%~dp0../../../3rd_party_libraries/OCCT" ^
     -D 3RDPARTY_DIR:PATH="%~dp0../../../3rd_party_libraries" ^
-    -D BUILD_SAMPLES_QT:BOOL="1" ^
-    -D 3RDPARTY_QT_DIR:PATH="%~dp0../../../../Qt\Qt5.12.11\5.12.11\msvc2017_64"
+    -D BUILD_SAMPLES_QT:BOOL="0" ^
+    -D 3RDPARTY_QT_DIR:PATH="%~dp0../../../../Qt/Qt5.12.11/5.12.11/msvc2017_64"
+cmake --build build --config Debug
+cmake --install build --config Debug
 pause
