33df371e6b3d736e83de87bde03e08d8eec95e05
liuxin <liuxin@fscut.com>
2023-07-21 13:09:35 +0800

    增加win32 debug生成。

---
+++ install_debug_win32.bat
@@ -0,0 +1,10 @@
+mkdir build
+cmake -S . -B build -A Win32 -D CMAKE_BUILD_TYPE=Debug -D BUILD_LIBRARY_TYPE:STRING="Static" ^
+    -D CMAKE_INSTALL_PREFIX:PATH="%~dp0../../../3rd_party_libraries/OCCT" ^
+    -D 3RDPARTY_DIR:PATH="%~dp0../../../3rd_party_libraries" ^
+    -D BUILD_SAMPLES_MFC:BOOL="0" -D BUILD_SAMPLES_QT:BOOL="0" ^
+    -D 3RDPARTY_QT_DIR:PATH="%~dp0../../../../Qt/Qt5.12.11/5.12.11/msvc2017_64" ^
+    -D USE_OPENGL:BOOL="1" -D USE_TK:BOOL="0" -D USE_FREETYPE:BOOL="1" -D BUILD_MODULE_Draw:BOOL="0"
+cmake --build build --config Debug
+cmake --install build --config Debug
+pause
